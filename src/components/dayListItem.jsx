b26d4da8e3bc41b2ace0f631f09f07542579a044
Francis <fwang36@gmail.com>
2022-03-22 13:27:33 -0400

    add props to daylistItem

--- src/components/dayListItem.jsx
+++ src/components/dayListItem.jsx
@@ -1,3 +1,15 @@
 //days:Array a list of day objects (each object includes an id, name, and spots)
 // day:String the currently selected day
 // setDay:Function accepts the name of the day eg. "Monday", "Tuesday"
+
+import React from "react";
+
+export default function DayListItem(props) {
+  console.log(props)
+  return (
+    <li onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3 className="text--light">{props.spots} spots remaining</h3>
+    </li>
+  );
+}
