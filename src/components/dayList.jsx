580b819c1c9515c739dd991fa08f08d6d1eac0b6
Francis <fwang36@gmail.com>
2022-03-22 13:14:22 -0400

    create dayList and dayListItem

---
+++ src/components/dayList.jsx
@@ -0,0 +1,4 @@
+// name:String the name of the day
+// spots:Number the number of spots remaining
+// selected:Boolean true or false declaring that this day is selected
+// setDay:Function accepts the name of the day eg. "Monday", "Tuesday"
---
+++ src/components/dayListItem.jsx
@@ -0,0 +1,3 @@
+//days:Array a list of day objects (each object includes an id, name, and spots)
+// day:String the currently selected day
+// setDay:Function accepts the name of the day eg. "Monday", "Tuesday"
