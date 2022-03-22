0892a417a97241a538a0345b2d2cdf6d65499ddf
Francis <fwang36@gmail.com>
2022-03-22 14:03:22 -0400

    add stories of DayList

--- src/components/dayList.jsx
+++ src/components/dayList.jsx
@@ -2,3 +2,16 @@
 // spots:Number the number of spots remaining
 // selected:Boolean true or false declaring that this day is selected
 // setDay:Function accepts the name of the day eg. "Monday", "Tuesday"
+
+import React from "react";
+import classNames from "classnames";
+import DayListItem from "./DayListItem";
+
+
+export default function Daylist(props) {
+
+  return (
+    <ul></ul>
+  )
+
+} 
