dd27335936d554f294c301953ea865215e84565e
Francis <fwang36@gmail.com>
2022-03-22 13:08:51 -0400

    add classNames

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,16 +2,19 @@ import React from "react";
 
 import "components/Button.scss";
 
+import classNames from "classnames";
+
+
 export default function Button(props) {
-   let buttonClass = "button";
+   let buttonClass = classNames("button",{'button--confirm': props.confirm}, {'button--danger': props.danger})
  
-   if (props.confirm) {
-     buttonClass += " button--confirm";
-   }
+   // if (props.confirm) {
+   //   buttonClass += " button--confirm";
+   // }
  
-   if (props.danger) {
-     buttonClass += " button--danger";
-   }
+   // if (props.danger) {
+   //   buttonClass += " button--danger";
+   // }
  
    return (
      <button
