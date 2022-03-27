3d16f1dd7a4f2dd61aeadf2dd6d2d3bd68805cdd
Francis <fwang36@gmail.com>
2022-03-27 18:15:08 -0400

    add useVisualMode and tests

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,14 +3,37 @@ import {React, useState} from 'react'
 export function useVisualMode(initial) {
 
   const [mode, setMode] = useState(initial)
+  const [history, setHistory] = useState([initial])
+  const transition = function(newMode, replace = false) {
+    if (replace) {
+      setMode((prev) => newMode)
+      let replaceHistory = [...history];
+      replaceHistory[replaceHistory.length - 1] = mode;
+      setHistory((prev) => prev)
 
-  const transition = function(newMode) {
-    setMode(newMode)
+    } else {
+      
+      setMode(prev => {
+        setMode(newMode)
+        if (prev !== initial) {
+          history.push(prev)
+        }
+  
+      })
+    }
+  }
+
+  const back = function() {
+    let newHistory = [...history]
+    setMode(newHistory[newHistory.length - 1])
+    newHistory.pop(mode)
+    setHistory(newHistory)
   }
 
   return {
     mode,
-    transition
+    transition,
+    back
   }
 }
 
