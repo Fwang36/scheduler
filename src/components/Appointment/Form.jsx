2eca8fc05a73de90600d2db506de28719c213828
Francis <fwang36@gmail.com>
2022-03-29 19:44:35 -0400

    add error message for no interviewer selected during form submission

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -24,6 +24,10 @@ export default function Form(props) {
       setError("Student name cannot be blank");
       return;
     }
+    if (!interviewer) {
+      setError("Please select interviewer")
+      return;
+    }
     props.onSave(student, interviewer, props.id);
   }
 
